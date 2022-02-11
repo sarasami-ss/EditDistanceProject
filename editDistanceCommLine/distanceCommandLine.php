a5c59c3d22291ad10fcb65d14d057ddec756f1a8
sarasami-ss <55747269+sarasami-ss@users.noreply.github.com>
2022-02-12 01:10:20 +0200

    Update distanceCommandLine.php

--- editDistanceCommLine/distanceCommandLine.php
+++ editDistanceCommLine/distanceCommandLine.php
@@ -4,10 +4,10 @@
 /**
  * User commnad line input.
  *
- * Description: This file created to display hamming and levenshtine distance
- * depeing on user inputs.
+ * Description: This file created to calculate hamming and levenshtine distance
+ * depeing on user inputs using command lines
  * 
- * @since 2/12/2021
+ * @since 2/12/2022
  */
 
 
@@ -15,15 +15,15 @@ require_once 'HammingDistance.php';
 require_once 'LevenshteinDistance.php';
 
 
-//Get user inputes from command
+//Get user inputs from command
 $firstString = readline("Enter the first string : ");
 $secondString = readline("Enter the second string : ");
 
-//Check if user not enter any values in CLI
+//Check empty values
 if (empty($firstString) || empty($secondString)) {
     exit("You need to enter string value to get your distance..!" . PHP_EOL);
 }
 
-//Echo results after find hamming and levenshtine distance
+//Echo results for hamming and levenshtine distance
 echo 'The hamming distance between ' . $firstString . ' and ' . $secondString . ' is => ' . HammingDistance::hamming_dis($firstString, $secondString) . PHP_EOL;
 echo 'The levenshtine distance between ' . $firstString . ' and ' . $secondString . ' is => ' . LevenshteinDistance::levenshtein_dis($firstString, $secondStringsa) . PHP_EOL;
